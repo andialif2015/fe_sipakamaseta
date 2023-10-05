bb6c458068cb358e3797b19c7a4f7c234f5167df
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-10-05 19:57:20 +0800

    FIX SUKET KEHILANGAN KK

--- src/app/administrasi/form/suketkehilangankk/inputItem.tsx
+++ src/app/administrasi/form/suketkehilangankk/inputItem.tsx
@@ -136,7 +136,19 @@ export default function InputItem() {
                     </label>
                     <input
                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        name="sekolah"
+                        name="nokk"
+                        type="text"
+                    />
+                </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-2">
+                <div className="w-full px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Alasan
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        name="alasan"
                         type="text"
                     />
                 </div>
