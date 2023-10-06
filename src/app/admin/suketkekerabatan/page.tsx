16ca6a21e028ec472e00aff63e507d872d00a5d1
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-10-06 15:34:02 +0800

    Menambahkan listView

---
+++ src/app/admin/suketkekerabatan/page.tsx
@@ -0,0 +1,44 @@
+import { prisma } from "@/../route"
+import { revalidatePath, } from "next/cache"
+
+
+export default async function Page() {
+    async function refreshData() {
+        'use server'
+        revalidatePath('/admin/imbsementara')
+    }
+
+    const postItem = await prisma.imbS.findMany({})
+
+    return (
+        <div className="min-h-screen ml-64 p-4">
+            <form action={refreshData}>
+                <button type="submit" className="bg-blue-500 p-4 mb-5 text-white rounded-xl">Update Data</button>
+            </form>
+            <table className="table-auto">
+                <thead className="bg-slate-700 text-md text-slate-400 ">
+                    <tr className="">
+                        <th className="p-3">Dibuat pada</th>
+                        <th className="p-3">Nama</th>
+                        <th className="p-3">NIK</th>
+                        <th className="p-3">Nomor HP</th>
+                        <th className="p-3">Status Surat</th>
+                        <th className="p-3">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {postItem.map((item) =>
+                        <tr className="bg-slate-500 text-white">
+                            <td className="p-3">{String(item.createdAt)}</td>
+                            <td className="p-3">{item.name}</td>
+                            <td className="p-3">{item.nik}</td>
+                            <td className="p-3">{item.nohp}</td>
+                            <td className="p-3">{String(item.status)}</td>
+                            <td className="p-3"><a href="#">Unduh</a></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
