339a1090fa818769aef9bf4ff191c10fc04f04db
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-10-09 08:59:52 +0800

    Hapus prisma

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,14 @@
-import {prisma} from "@/../route"
-
-
 export default async function Page() {
 
     async function setTrue(title: string) {
 
     }
 
-    const postItem = await prisma.post.findMany({})
-    // const  
+
 
     return (
         <div className="min-h-screen ml-64 p-4">
-            
+
         </div>
     )
 }
