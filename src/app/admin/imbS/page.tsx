03c008d18b70f68da56e5e950aea257c8b2fe77c
andialif <andialif2015@gmail.com>
2023-10-11 20:00:17 +0800

    (task): add download url in page imbs

--- src/app/admin/imbS/page.tsx
+++ src/app/admin/imbS/page.tsx
@@ -2,6 +2,7 @@
 import React from "react"
 import { revalidatePath, } from "next/cache"
 import { getAPI } from "@/utils/api"
+import { getBaseUrl } from "@/utils/helper";
 
 interface PostItem {
     id: string,
@@ -28,12 +29,13 @@ export default async function Page() {
     }
 
     const [postItem, setPostItem] = React.useState<PostItem[]>([]);
+    // const [baseUrl, setBaseUrl] = 
 
     React.useEffect(() => {
         const fetchData = async () => {
             try {
                 const resp = await getAPI("imbs/all", {});
-                if (resp.status) {
+                if (resp.data.status) {
                     setPostItem(resp.data.data);
                 }
             } catch (error) {
@@ -45,33 +47,38 @@ export default async function Page() {
         fetchData();
     }, []);
 
-    return (
-        <div className="min-h-screen ml-64 p-4">
+    function getDownloadURL(filename: string): string {
+      return getBaseUrl() + 'download?filename=' + filename;
+    }
 
-            <table className="table-auto">
-                <thead className="bg-slate-700 text-md text-slate-400 ">
-                    <tr className="">
-                        <th className="p-3">Dibuat pada</th>
-                        <th className="p-3">Nama</th>
-                        <th className="p-3">NIK</th>
-                        <th className="p-3">Nomor HP</th>
-                        <th className="p-3">Status Surat</th>
-                        <th className="p-3">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {postItem.map((item: any) =>
-                        <tr className="bg-slate-500 text-white">
-                            <td className="p-3">{String(item.createdAt)}</td>
-                            <td className="p-3">{item.name}</td>
-                            <td className="p-3">{item.nik}</td>
-                            <td className="p-3">{item.nohp}</td>
-                            <td className="p-3">{String(item.status)}</td>
-                            <td className="p-3"><a href="#">Unduh</a></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        </div>
-    )
+    return (
+      <div className="min-h-screen ml-64 p-4">
+        <table className="table-auto">
+          <thead className="bg-slate-700 text-md text-slate-400 ">
+            <tr className="">
+              <th className="p-3">Dibuat pada</th>
+              <th className="p-3">Nama</th>
+              <th className="p-3">NIK</th>
+              <th className="p-3">Nomor HP</th>
+              <th className="p-3">Status Surat</th>
+              <th className="p-3">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {postItem.map((item: any) => (
+              <tr className="bg-slate-500 text-white">
+                <td className="p-3">{String(item.createdAt)}</td>
+                <td className="p-3">{item.name}</td>
+                <td className="p-3">{item.nik}</td>
+                <td className="p-3">{item.nohp}</td>
+                <td className="p-3">{String(item.status)}</td>
+                <td className="p-3">
+                  <a href={getDownloadURL(item.fileName)}>Unduh</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
 }
--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,60 +1,10 @@
-import Docxtemplater from 'docxtemplater';
-import fs from 'fs';
-import path from 'path';
-import PizZip from 'pizzip';
-// import { prisma } from "@/../route";
 
 
-export async function generateWordDocument(data: FormData, templatePath: string, jenis: string, _id: number) {
-  try {
-
-    const templateContent = fs.readFileSync(templatePath, "binary");
-    const zip = new PizZip(templateContent);
-    const doc = new Docxtemplater(zip);
-    let dataForm = {};
-    if (jenis == 'spdaridesa') {
-      dataForm = setDataSPDesa(data);
-    }
-
-    doc.setData(dataForm);
-
-    doc.render();
-
-    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-    const fileName = `output_${timestamp}.docx`;
-
-    const outputPath = path.join(__dirname, `../../../../../../public/dokumen/${fileName}`);
-    const buffer = doc.getZip().generate({ type: 'nodebuffer' }); // Use generate() to get buffer
-
-    // await prisma.spfromdesa.update({
-    //   where: {
-    //     id: _id,
-    //   },
-    //   data: {
-    //     fileName: fileName
-    //   }
-    // });
-
-    fs.writeFileSync(outputPath, buffer); // Write the buffer to the file
-
-    return true;
-
-  } catch (error) {
-    throw error
-  }
-}
-
-function setDataSPDesa(data: FormData) {
-  return {
-    name: data.get('name') as string,
-    tempatlahir: data.get('tempatlahir') as string,
-    tgl: data.get('tgl') as string,
-    nik: data.get('nik') as string,
-    warga: data.get('warga') as string,
-    agama: data.get('agama') as string,
-    pekerjaan: data.get('pekerjaan') as string,
-    alamat: data.get('alamat') as string,
-    for: data.get('for') as string,
+export function getBaseUrl(){
+  if (process.env.NODE_ENV == "production") {
+    return process.env.API_URL_PROD;
+  } else {
+    return process.env.API_URL;
   }
 }
 
